ea223167197a119f03fb848b028c2398c2f5d1a0
hamza-qarar <q.hamza@protonmail.com>
2026-05-21 21:16:16 +0200

    Projekt aufgesetzt

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        body {
+            background-image: url('./hintergrund.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        h1 {
+            color: darkblue;
+            text-align: center;
+            margin-top: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Seite unter Bearbeitung</h1>
+</body>
+</html>
